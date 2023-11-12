c4873ba3802b39ece3a001f1afadfcc696bfbfc0
Simon Chapman <eatyourpeasapps@gmail.com>
2023-11-12 22:16:03 +0000

    add edit survey routes

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create-base-survey", views.create_base_survey, name="create_base_survey"),
     path("base-survey/<int:survey_id>/delete", views.delete_base_survey, name="delete_base_survey"),
+    path("base-survey/<int:survey_id>/edit", views.edit_base_survey, name="edit_base_survey"),
 ]
--- base/views.py
+++ base/views.py
@@ -38,3 +38,12 @@ def delete_base_survey(request, survey_id):
     return render(
         request=request, template_name="survey-table.html", context=context
     )
+
+def edit_base_survey(request, survey_id):
+    """
+    GET request to edit survey page
+    """
+    context = {"survey": BaseSurvey.objects.filter(pk=survey_id).get()}
+    return render(
+        request=request, template_name="edit-survey-table.html", context=context
+    )
